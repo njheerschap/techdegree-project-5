42510d8a6dd3b564ff334dcdd1b5cd396ed58882
njheerschap <78928590+njheerschap@users.noreply.github.com>
2021-03-16 20:42:40 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 baguetteBox.run('.gallery');
 
-let images = document.getElementsByTagName('a');
+const images = document.getElementsByTagName('a');
 const input = document.getElementById('searchBar');
 
 input.addEventListener('keyup', search => {
-    let searchString = search.target.value.toLowerCase();
+    const searchString = search.target.value.toLowerCase();
     for( i = 0; i < images.length; i++) {
-        let inputVal = images[i].getAttribute('data-caption');
+        const inputVal = images[i].getAttribute('data-caption');
         if (inputVal.toLocaleLowerCase().indexOf(searchString) > -1) {
             images[i].style.display = "";
         } else {
